9a2c72a717c05e915d0c3f7f45a5d20def990d5e
thuan.nv10 <thuan.nv10@samsung.com>
2024-01-03 17:07:37 +0700

    update config for render.

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ CDN_DOMAIN = f'http://127.0.0.1:{PORT}/'
 UPLOAD_DIR = '/home/pc/thuannv/upload_demo/'
 UPLOAD_DIR_DRIVE_ID = "1lMSdUNMreJIKi0livrRozvehCM-hWP-i"
 HOST_NAME = '0.0.0.0'
-SQLALCHEMY_DATABASE_URI = 'postgresql://thuannv:06121998@localhost:5432/flask_app'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://thuannv:06121998@localhost:5432/flask_app'
 # SQLALCHEMY_DATABASE_URI = 'postgresql://thuannv:06121998@172.17.0.3:5432/flask_app'
-
+# SQLALCHEMY_DATABASE_URI = 'postgresql://thuannv:XEAyCz7iUsS8cb3mcoU934SGozuBDpRG@dpg-cmais5ed3nmc73cqneog-a.oregon-postgres.render.com/flask_app_rxrz'
+SQLALCHEMY_DATABASE_URI = 'postgresql://thuannv:XEAyCz7iUsS8cb3mcoU934SGozuBDpRG@dpg-cmais5ed3nmc73cqneog-a/flask_app_rxrz'
