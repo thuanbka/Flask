34556adabd34df67177446f342e638dfa9274dc3
thuannv <thuan98.nv@gmail.com>
2024-12-29 23:58:36 +0700

    update version for requirement

--- config.py
+++ config.py
@@ -9,8 +9,7 @@ HOST_NAME = '0.0.0.0'
 # MYLAP
 # SQLALCHEMY_DATABASE_URI = 'postgresql://thuannv:thuan06121998@localhost:5432/flask_app'
 # RENDER-EXTERNAL
-SQLALCHEMY_DATABASE_URI = 'postgresql://flask_app_ldj6_user:croajE9BCAYTZa9n6AiwfUOg1u0TyXfh@dpg-ctomvr3qf0us73apheag-a.oregon-postgres.render.com:5432/flask_app_ldj6'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://flask_app_ldj6_user:croajE9BCAYTZa9n6AiwfUOg1u0TyXfh@dpg-ctomvr3qf0us73apheag-a.oregon-postgres.render.com:5432/flask_app_ldj6'
 # RENDER_INTERNAL
-# SQLALCHEMY_DATABASE_URI = 'postgresql://flask_app_ldj6_user:croajE9BCAYTZa9n6AiwfUOg1u0TyXfh@dpg-ctomvr3qf0us73apheag-a/flask_app_ldj6'
+SQLALCHEMY_DATABASE_URI = 'postgresql://flask_app_ldj6_user:croajE9BCAYTZa9n6AiwfUOg1u0TyXfh@dpg-ctomvr3qf0us73apheag-a/flask_app_ldj6'
 # SQLALCHEMY_DATABASE_URI = 'postgresql://thuannv:06121998@172.17.0.3:5432/flask_app'
-
